75bc118c48ede364d1360c07083ef78983b77193
Ali Abdolahi <a.abdollahi@1dws.com>
2012-02-03 18:32:09 +0330

    firefox bug fixed, some effects added just for MD :D

--- js/script.js
+++ js/script.js
@@ -2,7 +2,78 @@
 // loading part
 
 
+/////////////
+// background position have bug in FF and IE6 so, this jquery-plug-in added
+/////////////
 
+(function($) {
+	if(!document.defaultView || !document.defaultView.getComputedStyle){ // IE6-IE8
+		var oldCurCSS = $.curCSS;
+		$.curCSS = function(elem, name, force){
+			if(name === 'background-position'){
+				name = 'backgroundPosition';
+			}
+			if(name !== 'backgroundPosition' || !elem.currentStyle || elem.currentStyle[ name ]){
+				return oldCurCSS.apply(this, arguments);
+			}
+			var style = elem.style;
+			if ( !force && style && style[ name ] ){
+				return style[ name ];
+			}
+			return oldCurCSS(elem, 'backgroundPositionX', force) +' '+ oldCurCSS(elem, 'backgroundPositionY', force);
+		};
+	}
+	
+	var oldAnim = $.fn.animate;
+	$.fn.animate = function(prop){
+		if('background-position' in prop){
+			prop.backgroundPosition = prop['background-position'];
+			delete prop['background-position'];
+		}
+		if('backgroundPosition' in prop){
+			prop.backgroundPosition = '('+ prop.backgroundPosition;
+		}
+		return oldAnim.apply(this, arguments);
+	};
+	
+	function toArray(strg){
+		strg = strg.replace(/left|top/g,'0px');
+		strg = strg.replace(/right|bottom/g,'100%');
+		strg = strg.replace(/([0-9\.]+)(\s|\)|$)/g,"$1px$2");
+		var res = strg.match(/(-?[0-9\.]+)(px|\%|em|pt)\s(-?[0-9\.]+)(px|\%|em|pt)/);
+		return [parseFloat(res[1],10),res[2],parseFloat(res[3],10),res[4]];
+	}
+	
+	$.fx.step. backgroundPosition = function(fx) {
+		if (!fx.bgPosReady) {
+			var start = $.curCSS(fx.elem,'backgroundPosition');
+			if(!start){//FF2 no inline-style fallback
+				start = '0px 0px';
+			}
+			
+			start = toArray(start);
+			fx.start = [start[0],start[2]];
+			var end = toArray(fx.end);
+			fx.end = [end[0],end[2]];
+			
+			fx.unit = [end[1],end[3]];
+			fx.bgPosReady = true;
+		}
+		//return;
+		var nowPosX = [];
+		nowPosX[0] = ((fx.end[0] - fx.start[0]) * fx.pos) + fx.start[0] + fx.unit[0];
+		nowPosX[1] = ((fx.end[1] - fx.start[1]) * fx.pos) + fx.start[1] + fx.unit[1];           
+		fx.elem.style.backgroundPosition = nowPosX[0]+' '+nowPosX[1];
+
+	};
+})(jQuery);
+
+
+
+
+
+
+//////////// first loading
 
 $(window).load(function() {
 		typwriter('#slide-client',"$ 1Devs -m run GUI for 'loading'          ",0);
@@ -21,12 +92,14 @@ function guiloader(){
 		  },4000);	
 		  
 		  $('#slider .right_sec').delay(4000).animate({
-				'background-position-x':'0px'
+				'background-position-x':'0px',
+				'backgroundPosition' : '0px 84px'
 				},1000,function(){
 					$('.right_sec #brwsrs_msg').fadeIn(1000);
 		  });
 		  $('#slider .left_sec').delay(4000).animate({
-				'background-position-x':'305px'
+				'background-position-x':'305px',
+				'backgroundPosition' : '305px 84px'
 				},1000,function(){
 					$('.left_sec #mobs_msg').fadeIn(1000);
 		   });
